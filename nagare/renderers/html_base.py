d3eab3b8d2b7fae6c7b38891539d2281bc88a4a0
Alain Poirier <alain.poirier@net-ng.com>
2022-12-22 17:19:51 +0100

    No version added for `data:` url scheme

--- nagare/renderers/html_base.py
+++ nagare/renderers/html_base.py
@@ -35,8 +35,16 @@ from nagare.renderers.xml import TagProp
 componentattrs = {'id', 'class', 'style', 'title'}
 i18nattrs = {'lang', 'dir'}
 eventattrs = {
-    'onclick', 'ondblclick', 'onmousedown', 'onmouseup', 'onmousemove',
-    'onmouseover', 'onmouseout', 'onkeypress', 'onkeydown', 'onkeyup'
+    'onclick',
+    'ondblclick',
+    'onmousedown',
+    'onmouseup',
+    'onmousemove',
+    'onmouseover',
+    'onmouseout',
+    'onkeypress',
+    'onkeydown',
+    'onkeyup',
 }
 allattrs = componentattrs | i18nattrs | eventattrs
 focusattrs = {'accesskey', 'tabindex', 'onfocus', 'onblur'}
@@ -45,7 +53,6 @@ focusattrs = {'accesskey', 'tabindex', 'onfocus', 'onblur'}
 
 
 class Url(object):
-
     def __init__(self, url):
         self.url = url
         self.parts = list(urlparse.urlparse(url))
@@ -54,7 +61,7 @@ class Url(object):
         return bool(self.parts[0])
 
     def is_absolute(self):
-        return self.parts[2].startswith('/')
+        return self.parts[2].startswith('/') or (self.parts[0] == 'data')
 
     def absolute(self, url_prefix, always_relative=False, **params):
         """Convert a relative URL of a static content to an absolute one
@@ -79,12 +86,13 @@ class Url(object):
 
 def absolute_url(url, url_prefix, always_relative=False, **params):
     return Url(url).absolute(url_prefix, always_relative, **params)
+
+
 absolute_asset_url = absolute_url  # noqa: E305
 
 
 class Tag(xml.Tag):
-    """A html tag
-    """
+    """A html tag"""
 
     def tostring(self, method='html', encoding='utf-8', pipeline=True, **kw):
         """Serialize in HTML the tree beginning at this tag
@@ -125,7 +133,6 @@ class HrefAttribute(Tag):
 
 
 class Link(HrefAttribute):
-
     def on_change(self):
         if self.get('rel', '') in ('icon', 'stylesheet'):
             super(Link, self).on_change()
@@ -133,11 +140,12 @@ class Link(HrefAttribute):
 
 class SrcAttribute(HrefAttribute):
     ASSET_ATTR = 'src'
+
+
 Embed = Input = Script = SrcAttribute  # noqa: E305
 
 
 class Img(SrcAttribute):
-
     def on_change(self):
         super(Img, self).on_change()
 
@@ -258,54 +266,51 @@ class HeadRenderer(xml.XmlRenderer):
 
         head.extend(
             self.link(rel='stylesheet', type='text/css', href=url, **attributes)
-            for url, (attributes, bottom)
-            in self._css_url.items()
+            for url, (attributes, bottom) in self._css_url.items()
             if not bottom
         )
         head.extend(
             self.script(type='text/javascript', src=url, **attributes)
-            for url, (attributes, bottom)
-            in self._javascript_url.items()
+            for url, (attributes, bottom) in self._javascript_url.items()
             if not bottom
         )
 
         head.extend(
             self.style(css, type='text/css', data_nagare_css=name, **attributes)
-            for name, (css, attributes, bottom)
-            in self._named_css.items()
+            for name, (css, attributes, bottom) in self._named_css.items()
             if not bottom
         )
         head.extend(
             self.script(js, type='text/javascript', data_nagare_js=name, **attributes)
-            for name, (js, attributes, bottom)
-            in self._named_javascript.items()
+            for name, (js, attributes, bottom) in self._named_javascript.items()
             if not bottom
         )
 
         return head
 
     def render_bottom(self):
-        return [
-            self.link(rel='stylesheet', type='text/css', href=url, **attributes)
-            for url, (attributes, bottom)
-            in self._css_url.items()
-            if bottom
-        ] + [
-            self.script(type='text/javascript', src=url, **attributes)
-            for url, (attributes, bottom)
-            in self._javascript_url.items()
-            if bottom
-        ] + [
-            self.style(css, type='text/css', data_nagare_css=name, **attributes)
-            for name, (css, attributes, bottom)
-            in self._named_css.items()
-            if bottom
-        ] + [
-            self.script(js, type='text/javascript', data_nagare_js=name, **attributes)
-            for name, (js, attributes, bottom)
-            in self._named_javascript.items()
-            if bottom
-        ]
+        return (
+            [
+                self.link(rel='stylesheet', type='text/css', href=url, **attributes)
+                for url, (attributes, bottom) in self._css_url.items()
+                if bottom
+            ]
+            + [
+                self.script(type='text/javascript', src=url, **attributes)
+                for url, (attributes, bottom) in self._javascript_url.items()
+                if bottom
+            ]
+            + [
+                self.style(css, type='text/css', data_nagare_css=name, **attributes)
+                for name, (css, attributes, bottom) in self._named_css.items()
+                if bottom
+            ]
+            + [
+                self.script(js, type='text/javascript', data_nagare_js=name, **attributes)
+                for name, (js, attributes, bottom) in self._named_javascript.items()
+                if bottom
+            ]
+        )
 
 
 class Renderer(xml.XmlRenderer):
@@ -316,8 +321,16 @@ class Renderer(xml.XmlRenderer):
     componentattrs = {'id', 'class', 'style', 'title'}
     i18nattrs = {'lang', 'dir'}
     eventattrs = {
-        'onclick', 'ondblclick', 'onmousedown', 'onmouseup', 'onmousemove',
-        'onmouseover', 'onmouseout', 'onkeypress', 'onkeydown', 'onkeyup'
+        'onclick',
+        'ondblclick',
+        'onmousedown',
+        'onmouseup',
+        'onmousemove',
+        'onmouseover',
+        'onmouseout',
+        'onkeypress',
+        'onkeydown',
+        'onkeyup',
     }
     focusattrs = {'accesskey', 'tabindex', 'onfocus', 'onblur'}
     cellhalignattrs = {'align', 'char', 'charoff'}
@@ -327,27 +340,45 @@ class Renderer(xml.XmlRenderer):
     # The HTML tags
     # -------------
 
-    a = TagProp('a', allattrs | focusattrs | {
-        'charset', 'type', 'name', 'href', 'hreflang', 'rel',
-        'rev', 'shape', 'coords', 'target', 'oncontextmenu'
-    })
+    a = TagProp(
+        'a',
+        allattrs
+        | focusattrs
+        | {'charset', 'type', 'name', 'href', 'hreflang', 'rel', 'rev', 'shape', 'coords', 'target', 'oncontextmenu'},
+    )
     abbr = TagProp('abbr', allattrs)
     acronym = TagProp('acronym', allattrs)
     address = TagProp('address', allattrs)
-    applet = TagProp('applet', componentattrs | {
-        'codebase', 'archive', 'code', 'object', 'alt', 'name', 'width',
-        'height', 'align', 'hspace', 'vspace'
-    })
+    applet = TagProp(
+        'applet',
+        componentattrs
+        | {'codebase', 'archive', 'code', 'object', 'alt', 'name', 'width', 'height', 'align', 'hspace', 'vspace'},
+    )
     area = TagProp('area', allattrs | focusattrs | {'shape', 'coords', 'href', 'nohref', 'alt', 'target'})
     b = TagProp('b', allattrs)
     basefont = TagProp('basefont', componentattrs | i18nattrs | {'id', 'size', 'color', 'face'})
     bdo = TagProp('bdo', componentattrs | eventattrs | {'lang', 'dir'})
     big = TagProp('big', allattrs)
     blockquote = TagProp('blockquote', allattrs | {'cite'})
-    body = TagProp('body', allattrs | {
-        'onload', 'onunload', 'onfocus', 'background', 'bgcolor', 'text',
-        'link', 'vlink', 'alink', 'leftmargin', 'topmargin', 'marginwidth', 'marginheight'
-    })
+    body = TagProp(
+        'body',
+        allattrs
+        | {
+            'onload',
+            'onunload',
+            'onfocus',
+            'background',
+            'bgcolor',
+            'text',
+            'link',
+            'vlink',
+            'alink',
+            'leftmargin',
+            'topmargin',
+            'marginwidth',
+            'marginheight',
+        },
+    )
     br = TagProp('br', componentattrs | {'clear'})
     button = TagProp('button', allattrs | focusattrs | {'name', 'value', 'type', 'disabled'})
     caption = TagProp('caption', allattrs | {'align'})
@@ -364,21 +395,47 @@ class Renderer(xml.XmlRenderer):
     dl = TagProp('dl', allattrs | {'compact'})
     dt = TagProp('dt', allattrs)
     em = TagProp('em', allattrs)
-    embed = TagProp('embed', {
-        'width', 'height', 'src', 'controller', 'src', 'target',
-        'border', 'pluginspage', 'quality', 'type', 'bgcolor', 'menu'
-    }, Embed)
+    embed = TagProp(
+        'embed',
+        {
+            'width',
+            'height',
+            'src',
+            'controller',
+            'src',
+            'target',
+            'border',
+            'pluginspage',
+            'quality',
+            'type',
+            'bgcolor',
+            'menu',
+        },
+        Embed,
+    )
     fieldset = TagProp('fieldset', allattrs)
     font = TagProp('font', componentattrs | i18nattrs | {'face', 'size', 'color'})
-    form = TagProp('form', allattrs | {
-        'action', 'method', 'name', 'enctype',
-        'onsubmit', 'onreset', 'accept_charset', 'target'
-    })
+    form = TagProp(
+        'form', allattrs | {'action', 'method', 'name', 'enctype', 'onsubmit', 'onreset', 'accept_charset', 'target'}
+    )
     frame = TagProp('frame', set())
-    frameset = TagProp('frameset', componentattrs | {
-        'rows', 'cols', 'onload', 'onunload', 'framespacing', 'border',
-        'marginwidth', 'marginheight', 'frameborder', 'noresize', 'scrolling'
-    })
+    frameset = TagProp(
+        'frameset',
+        componentattrs
+        | {
+            'rows',
+            'cols',
+            'onload',
+            'onunload',
+            'framespacing',
+            'border',
+            'marginwidth',
+            'marginheight',
+            'frameborder',
+            'noresize',
+            'scrolling',
+        },
+    )
     h1 = TagProp('h1', allattrs | {'align'})
     h2 = TagProp('h2', allattrs | {'align'})
     h3 = TagProp('h3', allattrs | {'align'})
@@ -388,18 +445,70 @@ class Renderer(xml.XmlRenderer):
     hr = TagProp('hr', allattrs | {'align', 'noshade', 'size', 'width', 'color'})
     html = TagProp('html', i18nattrs | {'id'})
     i = TagProp('i', allattrs)
-    iframe = TagProp('iframe', componentattrs | {
-        'longdesc', 'name', 'src', 'frameborder', 'marginwidth', 'marginheight',
-        'noresize', 'scrolling', 'align', 'height', 'width', 'hspace', 'vspace', 'bordercolor',
-    })
-    img = TagProp('img', allattrs | {
-        'src', 'alt', 'name', 'longdesc', 'width', 'height', 'usemap',
-        'ismap', 'align', 'border', 'hspace', 'vspace', 'lowsrc'
-    }, Img)
-    input = TagProp('input', allattrs | focusattrs | {
-        'type', 'name', 'value', 'checked', 'disabled', 'readonly', 'size', 'maxlength',
-        'src', 'alt', 'usemap', 'onselect', 'onchange', 'accept', 'align', 'border'
-    }, Input)
+    iframe = TagProp(
+        'iframe',
+        componentattrs
+        | {
+            'longdesc',
+            'name',
+            'src',
+            'frameborder',
+            'marginwidth',
+            'marginheight',
+            'noresize',
+            'scrolling',
+            'align',
+            'height',
+            'width',
+            'hspace',
+            'vspace',
+            'bordercolor',
+        },
+    )
+    img = TagProp(
+        'img',
+        allattrs
+        | {
+            'src',
+            'alt',
+            'name',
+            'longdesc',
+            'width',
+            'height',
+            'usemap',
+            'ismap',
+            'align',
+            'border',
+            'hspace',
+            'vspace',
+            'lowsrc',
+        },
+        Img,
+    )
+    input = TagProp(
+        'input',
+        allattrs
+        | focusattrs
+        | {
+            'type',
+            'name',
+            'value',
+            'checked',
+            'disabled',
+            'readonly',
+            'size',
+            'maxlength',
+            'src',
+            'alt',
+            'usemap',
+            'onselect',
+            'onchange',
+            'accept',
+            'align',
+            'border',
+        },
+        Input,
+    )
     ins = TagProp('ins', allattrs | {'cite', 'datetime'})
     isindex = TagProp('isindex', componentattrs | i18nattrs | {'prompt'})
     kbd = TagProp('kbd', allattrs)
@@ -410,11 +519,29 @@ class Renderer(xml.XmlRenderer):
     menu = TagProp('menu', allattrs | {'compact'})
     noframes = TagProp('noframes', allattrs)
     noscript = TagProp('noscript', allattrs)
-    object = TagProp('object', allattrs | {
-        'declare', 'classid', 'codebase', 'data', 'type', 'codetype',
-        'archive', 'standby', 'height', 'width', 'usemap', 'name',
-        'tabindex', 'align', 'border', 'hspace', 'vspace'
-    })
+    object = TagProp(
+        'object',
+        allattrs
+        | {
+            'declare',
+            'classid',
+            'codebase',
+            'data',
+            'type',
+            'codetype',
+            'archive',
+            'standby',
+            'height',
+            'width',
+            'usemap',
+            'name',
+            'tabindex',
+            'align',
+            'border',
+            'hspace',
+            'vspace',
+        },
+    )
     ol = TagProp('ol', allattrs | {'type', 'compact', 'start'})
     optgroup = TagProp('optgroup', allattrs | {'disabled', 'label'})
     option = TagProp('option', allattrs | {'selected', 'disabled', 'label', 'value'})
@@ -425,10 +552,10 @@ class Renderer(xml.XmlRenderer):
     s = TagProp('s', allattrs)
     samp = TagProp('samp', allattrs)
     script = TagProp('script', {'id', 'charset', 'type', 'language', 'src', 'defer'}, Script)
-    select = TagProp('select', allattrs | {
-        'name', 'size', 'multiple', 'disabled', 'tabindex',
-        'onfocus', 'onblur', 'onchange', 'rows'
-    })
+    select = TagProp(
+        'select',
+        allattrs | {'name', 'size', 'multiple', 'disabled', 'tabindex', 'onfocus', 'onblur', 'onchange', 'rows'},
+    )
     small = TagProp('small', allattrs)
     span = TagProp('span', allattrs)
     strike = TagProp('strike', allattrs)
@@ -438,21 +565,50 @@ class Renderer(xml.XmlRenderer):
     sup = TagProp('sup', allattrs)
     table = TagProp('table', componentattrs | i18nattrs | {'prompt'})
     tbody = TagProp('tbody', allattrs | cellhalignattrs | cellvalignattrs)
-    td = TagProp('td', allattrs | cellhalignattrs | cellvalignattrs | {
-        'abbr', 'axis', 'headers', 'scope', 'rowspan',
-        'colspan', 'nowrap', 'bgcolor', 'width', 'height',
-        'background', 'bordercolor'
-    })
-    textarea = TagProp('textarea', allattrs | focusattrs | {
-        'name', 'rows', 'cols', 'disabled',
-        'readonly', 'onselect', 'onchange', 'wrap'
-    })
+    td = TagProp(
+        'td',
+        allattrs
+        | cellhalignattrs
+        | cellvalignattrs
+        | {
+            'abbr',
+            'axis',
+            'headers',
+            'scope',
+            'rowspan',
+            'colspan',
+            'nowrap',
+            'bgcolor',
+            'width',
+            'height',
+            'background',
+            'bordercolor',
+        },
+    )
+    textarea = TagProp(
+        'textarea',
+        allattrs | focusattrs | {'name', 'rows', 'cols', 'disabled', 'readonly', 'onselect', 'onchange', 'wrap'},
+    )
     tfoot = TagProp('tfoot', allattrs | cellhalignattrs | cellvalignattrs)
-    th = TagProp('th', allattrs | cellhalignattrs | cellvalignattrs | {
-        'abbr', 'axis', 'headers', 'scope', 'rowspan',
-        'colspan', 'nowrap', 'bgcolor', 'width', 'height'
-        'background', 'bordercolor'
-    })
+    th = TagProp(
+        'th',
+        allattrs
+        | cellhalignattrs
+        | cellvalignattrs
+        | {
+            'abbr',
+            'axis',
+            'headers',
+            'scope',
+            'rowspan',
+            'colspan',
+            'nowrap',
+            'bgcolor',
+            'width',
+            'height' 'background',
+            'bordercolor',
+        },
+    )
     thead = TagProp('thead', allattrs | cellhalignattrs | cellvalignattrs)
     tr = TagProp('tr', allattrs | cellhalignattrs | cellvalignattrs | {'bgcolor', 'nowrap', 'width', 'background'})
     tt = TagProp('tt', allattrs)
@@ -460,9 +616,9 @@ class Renderer(xml.XmlRenderer):
     ul = TagProp('ul', allattrs | {'type', 'compact'})
     var = TagProp('var', allattrs)
 
-#    frame = TagProp('frame', componentattrs | {'longdesc', 'name', 'src', 'frameborder', 'marginwidht', 'marginheight',
-#                                                                  'noresize', 'scrolling', 'framespacing', 'border', 'marginwidth', 'marginheight',
-#                                                                  'frameborder', 'noresize', 'scrolling'}, SrcAttribute)
+    #    frame = TagProp('frame', componentattrs | {'longdesc', 'name', 'src', 'frameborder', 'marginwidht', 'marginheight',
+    #                                                                  'noresize', 'scrolling', 'framespacing', 'border', 'marginwidth', 'marginheight',
+    #                                                                  'frameborder', 'noresize', 'scrolling'}, SrcAttribute)
     _parser = ET.HTMLParser()
     _parser.set_element_class_lookup(ET.ElementDefaultClassLookup(element=Tag))
 
