008269a42197ac9d09e21eddbd36bc9fb397b07d
Alain Poirier <alain.poirier@net-ng.com>
2026-02-03 23:08:18 +0100

    Python 3

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -11,114 +11,113 @@ import os
 from io import StringIO
 
 import pytest
+from lxml.etree import XMLSyntaxError
 
-from nagare.renderers import html_base as html
 from nagare.renderers import xml
+from nagare.renderers import html_base as html
 
 
 def test_parse1():
     h = html.HeadRenderer()
 
-    root = h.fromfile(StringIO("<html><body/></html>"))
+    root = h.fromfile(StringIO('<html><body/></html>'))
     assert isinstance(root, html.Tag)
-    assert root.tostring() == b"<html><body></body></html>"
+    assert root.tostring() == b'<html><body></body></html>'
 
-    root = h.fromfile(StringIO("<html><body/></html>"), xml.Tag)
+    root = h.fromfile(StringIO('<html><body/></html>'), xml.Tag)
     assert isinstance(root, xml.Tag)
-    assert root.tostring() == b"<html><body/></html>"
+    assert root.tostring() == b'<html><body/></html>'
 
-    root = h.fromstring("<html><body/></html>")
+    root = h.fromstring('<html><body/></html>')
     assert isinstance(root, html.Tag)
-    assert root.tostring() == b"<html><body></body></html>"
+    assert root.tostring() == b'<html><body></body></html>'
 
-    root = h.fromstring("<html><body/></html>", xml.Tag)
+    root = h.fromstring('<html><body/></html>', xml.Tag)
     assert isinstance(root, xml.Tag)
-    assert root.tostring() == b"<html><body/></html>"
+    assert root.tostring() == b'<html><body/></html>'
 
 
 def test_parse2():
     h = html.Renderer()
 
-    root = h.fromfile(StringIO("<html><body/></html>"))
+    root = h.fromfile(StringIO('<html><body/></html>'))
     assert isinstance(root, html.Tag)
-    assert root.tostring() == b"<html><body></body></html>"
+    assert root.tostring() == b'<html><body></body></html>'
 
-    root = h.fromfile(StringIO("<html><body/></html>"), xml.Tag)
+    root = h.fromfile(StringIO('<html><body/></html>'), xml.Tag)
     assert isinstance(root, xml.Tag)
-    assert root.tostring() == b"<html><body/></html>"
+    assert root.tostring() == b'<html><body/></html>'
 
-    root = h.fromstring("<html><body/></html>")
+    root = h.fromstring('<html><body/></html>')
     assert isinstance(root, html.Tag)
-    assert root.tostring() == b"<html><body></body></html>"
+    assert root.tostring() == b'<html><body></body></html>'
 
-    root = h.fromstring("<html><body/></html>", xml.Tag)
+    root = h.fromstring('<html><body/></html>', xml.Tag)
     assert isinstance(root, xml.Tag)
-    assert root.tostring() == b"<html><body/></html>"
+    assert root.tostring() == b'<html><body/></html>'
 
 
 def test_parse3():
-    """XHTML namespace unit test - HTMLRender - parse_html - bad encoding"""
+    """XHTML namespace unit test - HTMLRender - parse_html - bad encoding."""
     h = html.Renderer()
-    filename = os.path.join(os.path.dirname(__file__), "iso-8859.xml")
+    filename = os.path.join(os.path.dirname(__file__), 'iso-8859.xml')
 
-    with pytest.raises(UnicodeDecodeError):
-        h.fromfile(filename, encoding="utf-8")
+    with pytest.raises(XMLSyntaxError):
+        h.fromfile(filename, encoding='utf-8', recover=False)
 
-    h.fromfile(filename, encoding="iso8859-1")
+    h.fromfile(filename, encoding='iso8859-1', recover=False)
 
 
 def test_parse4():
     h = html.Renderer()
-    root = h.fromstring("<html><head><body></body></head><html>")
-    assert root.tostring() == b"<html><head></head><body></body></html>"
+    root = h.fromstring('<html><head><body></body></head><html>')
+    assert root.tostring() == b'<html><head></head><body></body></html>'
 
 
 def test_parse5():
     h = html.Renderer()
-    root = h.fromstring("test")
-    assert root.tostring() == b"<html><body>test</body></html>"
+    root = h.fromstring('test')
+    assert root.tostring() == b'<html><body>test</body></html>'
 
 
 def test_parse6():
     h = html.Renderer()
-    root = h.fromstring("<a>text</a>")
+    root = h.fromstring('<a>text</a>')
     assert type(root) is html.Tag
 
     x = xml.Renderer()
-    root = x.fromstring("<a>text</a>")
+    root = x.fromstring('<a>text</a>')
     assert type(root) is xml.Tag
 
 
 def test_parse8():
     h = html.Renderer()
-    root = h.fromstring("<a>text</a>", fragment=True)
-    assert isinstance(root, tuple)
+    root = h.fromstring('<a>text</a>', fragment=True)
+    assert isinstance(root, list)
     assert len(root) == 1
-    assert root[0].tostring() == b"<a>text</a>"
+    assert root[0].tostring() == b'<a>text</a>'
 
 
 def test_parse9():
     h = html.Renderer()
-    root = h.fromstring("<a>text</a><b>text</b>", fragment=True)
-    assert isinstance(root, tuple)
+    root = h.fromstring('<a>text</a><b>text</b>', fragment=True)
+    assert isinstance(root, list)
     assert len(root) == 2
-    assert root[0].tostring() == b"<a>text</a>"
-    assert root[1].tostring() == b"<b>text</b>"
+    assert root[0].tostring() == b'<a>text</a>'
+    assert root[1].tostring() == b'<b>text</b>'
 
 
 def test_parse10():
     h = html.Renderer()
-    root = h.fromstring("hello<a>text</a><b>text</b>", fragment=True)
-    assert isinstance(root, tuple)
+    root = h.fromstring('hello<a>text</a><b>text</b>', fragment=True)
+    assert isinstance(root, list)
     assert len(root) == 3
-    assert root[0] == b"hello"
-    assert root[1].tostring() == b"<a>text</a>"
-    assert root[2].tostring() == b"<b>text</b>"
+    assert root[0] == 'hello'
+    assert root[1].tostring() == b'<a>text</a>'
+    assert root[2].tostring() == b'<b>text</b>'
 
-    root = h.fromstring(
-        "hello<a>text</a><b>text</b>", fragment=True, no_leading_text=True
-    )
-    assert isinstance(root, tuple)
+    root = h.fromstring('hello<a>text</a><b>text</b>', fragment=True, no_leading_text=True)
+    assert isinstance(root, list)
     assert len(root) == 2
-    assert root[0].tostring() == b"<a>text</a>"
-    assert root[1].tostring() == b"<b>text</b>"
+    assert root[0].tostring() == b'<a>text</a>'
+    assert root[1].tostring() == b'<b>text</b>'
